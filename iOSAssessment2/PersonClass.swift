60864262121cbeed5cf6e3e5f6d39dc6f6ac63d9
Caleb Ogles <calebogles@MacBook-Pro-100.local>
2018-10-25 11:11:53 -0500

    Finished assessment

---
+++ iOSAssessment2/PersonClass.swift
@@ -0,0 +1,22 @@
+//
+//  PersonClass.swift
+//  iOSAssessment2
+//
+//  Created by Caleb Ogles on 10/25/18.
+//  Copyright © 2018 Caleb Ogles. All rights reserved.
+//
+
+import Foundation
+
+class Person {
+    var firstName: String
+    var lastName: String
+    var email: String
+    
+    init(firstName: String, lastName: String, email: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+    }
+    
+}
--- iOSAssessment2/ViewController.swift
+++ iOSAssessment2/ViewController.swift
@@ -9,12 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var newPerson: Person?
+    
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    
+    @IBAction func submitTapped(_ sender: Any) {
+        
+        
+        guard
+            let firstNameEmpty = firstNameTextField.text, !firstNameEmpty.isEmpty,
+            let lastNameEmpty = lastNameTextField.text, !lastNameEmpty.isEmpty,
+            let emailEmpty = emailTextField.text, !emailEmpty.isEmpty
+        
+            else {
+                let errorAlert = UIAlertController(title: "Error!", message: "You have not entered the information required fully to create your account", preferredStyle: UIAlertController.Style.alert)
+                let dismissAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default) {UIAlertAction in};
+                    errorAlert.addAction(dismissAction)
+                self.present(errorAlert, animated: true, completion: nil)
+                return
+        }
+        newPerson = Person(firstName: firstNameEmpty, lastName: lastNameEmpty, email: emailEmpty)
+        
+        let success = UIAlertController(title: "Greetings!!!", message: "Hello \(firstNameEmpty) \(lastNameEmpty)! Your email is \(emailEmpty).", preferredStyle: .alert)
+        let okayAction = UIAlertAction(title: "Thanks!", style: UIAlertAction.Style.default) {UIAlertAction in self.newPerson
+        }
+        success.addAction(okayAction)
+        self.present(success, animated: true, completion: nil)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing(_:))))
     }
-
+    
 
 }
+
 
