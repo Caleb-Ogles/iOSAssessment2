c2eef237d482b16bc39477c460c9fe0a37b0420c
Caleb Ogles <calebogles@MacBook-Pro-100.local>
2018-10-25 10:00:02 -0500

    Initial Commit

---
+++ iOSAssessment2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSAssessment2
+//
+//  Created by Caleb Ogles on 10/25/18.
+//  Copyright © 2018 Caleb Ogles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
